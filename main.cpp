956f728235bcdf1e2d2044d5f90b7036aec0fe93
Naser <sir.naser10@proton.me>
2025-12-16 17:21:31 +0330

    <3

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
+// #include <QApplication>
+// #include "ui/splashscreen.h"
+
+// int main(int argc, char *argv[])
+// {
+//     QApplication a(argc, argv);
+
+//     SplashScreen splash;
+//     splash.show();
+
+//     return a.exec();
+// }
+
+#include "mainwindow.h"
+
 #include <QApplication>
-#include "ui/splashscreen.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
-    SplashScreen splash;
-    splash.show();
-
+    MainWindow w;
+    w.show();
     return a.exec();
+    // hello
 }
