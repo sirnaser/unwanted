3bc4573044f26d068c8e566747262ac27f70a002
Naser <sir.naser10@proton.me>
2025-12-16 17:16:30 +0330

    <3

--- gamewindow.cpp
+++ gamewindow.cpp
@@ -1,3 +1,23 @@
-#include "gamewindow.h"
+#include "GameWindow.h"
+#include "ui_GameWindow.h"
+#include "core/Player.h"
 
-GameWindow::GameWindow() {}
+GameWindow::GameWindow(const QString &p1,
+                       const QString &p2,
+                       QWidget *parent)
+    : QWidget(parent),
+    ui(new Ui::GameWindow)
+{
+    ui->setupUi(this);
+
+    Player player1(p1);
+    Player player2(p2);
+
+    gameManager = new GameManager(player1, player2);
+}
+
+GameWindow::~GameWindow()
+{
+    delete gameManager;
+    delete ui;
+}
