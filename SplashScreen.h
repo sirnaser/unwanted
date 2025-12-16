dbcb96c8b3e95604a9c2794828ac2ded98855a7b
Naser <sir.naser10@proton.me>
2025-12-16 16:58:13 +0330

    <3

---
+++ SplashScreen.h
@@ -0,0 +1,25 @@
+#ifndef SPLASHSCREEN_H
+#define SPLASHSCREEN_H
+
+#include <QWidget>
+
+namespace Ui {
+class SplashScreen;
+}
+
+class SplashScreen : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit SplashScreen(QWidget *parent = nullptr);
+    ~SplashScreen();
+
+private slots:
+    void on_startButton_clicked();
+
+private:
+    Ui::SplashScreen *ui;
+};
+
+#endif
